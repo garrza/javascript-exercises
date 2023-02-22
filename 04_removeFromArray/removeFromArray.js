d36b549284f288270c99620928a879f3c013db9c
Ramiro Garza <garza.ramirov@gmail.com>
2023-02-22 16:03:29 -0600

    Including and working with three dots to add optional parameters, using exclamation mark as a NOT operator, saving time and effort

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+          }
+    });
+        return newArray;
+    };
 
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
+    // #2 way
+    // let removeArray = function(array, ...args){
+    //     return array.filter(val => !args.includes(val))
+    // }
+    // Do not edit below this line
+    module.exports = removeFromArray;
