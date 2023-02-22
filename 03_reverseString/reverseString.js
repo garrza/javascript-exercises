a48938118dcd3a51faed8990ed92fffc1fdc9247
Ramiro Garza <garza.ramirov@gmail.com>
2023-02-22 15:17:05 -0600

    Create different versions that give same result, implementation of methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");;
 };
+
+// let newstring = "";
+// for(let i = string.length - 1;i >=;i--){
+//     newstring += string[i];
+// }
+// return newstring;
 
 // Do not edit below this line
 module.exports = reverseString;
