ccd7c1016283ce7d2e64d902239a774a7c869fbb
Ramiro Garza <garza.ramirov@gmail.com>
2023-02-22 16:39:55 -0600

    For loops, using different methods for optimizing code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    min = Math.min(n1,n2);
+    max = Math.max(n1,n2);
+    let sum = 0;
+    if(typeof n1 === "string" || typeof n2 === "string") return "ERROR";
+    else if(isNaN(n1) || isNaN(n2)) return "ERROR";
+    else if(n1 < 0 || n2 < 0) return "ERROR";
+    for (min;min <= max;min ++){
+        sum += min
+    }
+    return sum;
 };
 
 // Do not edit below this line
