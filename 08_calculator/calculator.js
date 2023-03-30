6e7a33e467e8dc77f44acfe8269b4a36e0795344
Ramiro Garza <garza.ramirov@gmail.com>
2023-03-30 07:28:25 -0600

    Complete calculator model functions working

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,value) => {
+    return total + value;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  for (let i=num-1; i >= 1; i --){
+    num = num * i;
+  }
+  return num === 0 || num < 0 ? 1 : num;
 };
 
 // Do not edit below this line
